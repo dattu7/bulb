5b9d9d6e8030008648c64f3555fafd4c4e092c34
Gundumolu Dattatreya <126433896+dattu7@users.noreply.github.com>
2023-07-19 15:56:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO9yLzruDXvlHE_-2pW7Iir9XMykEWSgnVRg&usqp=CAU";
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd5modKw8BIo_WTasIpStIz6RCoVNR2V2xOw&usqp=CAU";
+}
